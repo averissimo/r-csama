6d5036258613a043dce82f1ed7baa5518258be94
André Veríssimo <afsverissimo@gmail.com>
2016-07-12 15:19:11 +0200

    continued tutorial

--- lab-1.3.R
+++ lab-1.3.R
@@ -1,12 +1,56 @@
 
 #
 #
-# 1:
+# 2:
 
 library(IRanges)
 ir <- IRanges(start=c(10, 20, 30), width=5)
 ir
 
 flank(ir, 3)
+flank(ir, 3, start = FALSE)
+
+class(ir)
+
+getClass(class(ir))
+
+?"flank,Ranges-method" 
+
+#
+
+library(GenomicRanges)
+gr <- GRanges(c("chr1", "chr1", "chr2"), ir, strand=c("+", "-", "+"))
+gr
+
+flank(gr, 3)
+
+getClass(gr)
+class(gr)
+
+?"flank,GenomicRanges-method"
+
+#
+
+showMethods(class="GRanges", where=search())
+
+#
+
+help(package="GenomicRanges")
+vignette(package="GenomicRanges")
+vignette(package="GenomicRanges", "GenomicRangesHOWTOs")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
